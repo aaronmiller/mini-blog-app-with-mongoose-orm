21cdc32207936a82fee78ccd372cd8f372431303
Aaron <amiller719@gmail.com>
2017-07-08 23:00:28 -0400

    Changing database name

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 1337;
 const DATABASE_URL = process.env.DATABASE_URL ||
                      global.DATABASE_URL ||
-                     'mongodb://localhost/blog-app';
+                     'mongodb://localhost/mini-blog-app';
 
 module.exports = {
   HOST,
